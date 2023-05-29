78d7921197c688181a3b6efdc7f4043821a49e15
Yukari <34794115+sorae42@users.noreply.github.com>
2023-05-29 13:55:19 +0700

    resize component

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -31,7 +31,7 @@
             Label label2;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             Label label1;
-            tabControl1 = new TabControl();
+            appTabs = new TabControl();
             addFundTab = new TabPage();
             fundSpentInput = new NumericUpDown();
             addSpendingBtn = new Button();
@@ -41,13 +41,31 @@
             deleteBtn = new Button();
             spendingList = new ListBox();
             dateChooser = new DateTimePicker();
+            preferencesTab = new TabPage();
+            groupBox3 = new GroupBox();
+            label3 = new Label();
+            defaultSpendingNameInput = new TextBox();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            label5 = new Label();
+            spendingLimitInput = new NumericUpDown();
+            groupBox1 = new GroupBox();
+            autoLaunchTab = new RadioButton();
+            label6 = new Label();
+            radioButton2 = new RadioButton();
+            button1 = new Button();
             noticeLabel = new Label();
             label2 = new Label();
             label1 = new Label();
-            tabControl1.SuspendLayout();
+            appTabs.SuspendLayout();
             addFundTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)fundSpentInput).BeginInit();
             historyTab.SuspendLayout();
+            preferencesTab.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)spendingLimitInput).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label2
@@ -60,13 +78,14 @@
             resources.ApplyResources(label1, "label1");
             label1.Name = "label1";
             // 
-            // tabControl1
+            // appTabs
             // 
-            tabControl1.Controls.Add(addFundTab);
-            tabControl1.Controls.Add(historyTab);
-            resources.ApplyResources(tabControl1, "tabControl1");
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
+            appTabs.Controls.Add(addFundTab);
+            appTabs.Controls.Add(historyTab);
+            appTabs.Controls.Add(preferencesTab);
+            resources.ApplyResources(appTabs, "appTabs");
+            appTabs.Name = "appTabs";
+            appTabs.SelectedIndex = 0;
             // 
             // addFundTab
             // 
@@ -133,6 +152,95 @@
             resources.ApplyResources(dateChooser, "dateChooser");
             dateChooser.Name = "dateChooser";
             // 
+            // preferencesTab
+            // 
+            preferencesTab.Controls.Add(groupBox3);
+            preferencesTab.Controls.Add(groupBox2);
+            preferencesTab.Controls.Add(groupBox1);
+            preferencesTab.Controls.Add(button1);
+            resources.ApplyResources(preferencesTab, "preferencesTab");
+            preferencesTab.Name = "preferencesTab";
+            preferencesTab.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(defaultSpendingNameInput);
+            groupBox3.Controls.Add(label4);
+            resources.ApplyResources(groupBox3, "groupBox3");
+            groupBox3.Name = "groupBox3";
+            groupBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // defaultSpendingNameInput
+            // 
+            resources.ApplyResources(defaultSpendingNameInput, "defaultSpendingNameInput");
+            defaultSpendingNameInput.Name = "defaultSpendingNameInput";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.Name = "label4";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(spendingLimitInput);
+            resources.ApplyResources(groupBox2, "groupBox2");
+            groupBox2.Name = "groupBox2";
+            groupBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(label5, "label5");
+            label5.Name = "label5";
+            // 
+            // spendingLimitInput
+            // 
+            spendingLimitInput.Increment = new decimal(new int[] { 50000, 0, 0, 0 });
+            resources.ApplyResources(spendingLimitInput, "spendingLimitInput");
+            spendingLimitInput.Maximum = new decimal(new int[] { 1215752191, 23, 0, 0 });
+            spendingLimitInput.Name = "spendingLimitInput";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(autoLaunchTab);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(radioButton2);
+            resources.ApplyResources(groupBox1, "groupBox1");
+            groupBox1.Name = "groupBox1";
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // autoLaunchTab
+            // 
+            resources.ApplyResources(autoLaunchTab, "autoLaunchTab");
+            autoLaunchTab.Name = "autoLaunchTab";
+            autoLaunchTab.TabStop = true;
+            autoLaunchTab.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            resources.ApplyResources(label6, "label6");
+            label6.Name = "label6";
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(radioButton2, "radioButton2");
+            radioButton2.Name = "radioButton2";
+            radioButton2.TabStop = true;
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // noticeLabel
             // 
             resources.ApplyResources(noticeLabel, "noticeLabel");
@@ -144,23 +252,31 @@
             AutoScaleMode = AutoScaleMode.Font;
             CausesValidation = false;
             Controls.Add(noticeLabel);
-            Controls.Add(tabControl1);
+            Controls.Add(appTabs);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "MainWindow";
             ShowIcon = false;
             Load += Form1_Load;
-            tabControl1.ResumeLayout(false);
+            appTabs.ResumeLayout(false);
             addFundTab.ResumeLayout(false);
             addFundTab.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)fundSpentInput).EndInit();
             historyTab.ResumeLayout(false);
+            preferencesTab.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)spendingLimitInput).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private TabControl tabControl1;
+        private TabControl appTabs;
         private TabPage addFundTab;
         private TabPage historyTab;
         private MonthCalendar dateSelector;
@@ -173,5 +289,18 @@
         private DateTimePicker dateChooser;
         private NumericUpDown fundSpentInput;
         private Label noticeLabel;
+        private TabPage preferencesTab;
+        private Label label3;
+        private Label label4;
+        private TextBox defaultSpendingNameInput;
+        private NumericUpDown spendingLimitInput;
+        private Label label5;
+        private RadioButton radioButton2;
+        private RadioButton autoLaunchTab;
+        private Label label6;
+        private Button button1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox2;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -1,4 +1,4 @@
-namespace QuanLiTaiChinh
+﻿namespace QuanLiTaiChinh
 {
     public partial class MainWindow : Form
     {
@@ -9,7 +9,7 @@ namespace QuanLiTaiChinh
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void addSpendingBtn_Click(object sender, EventArgs e)
@@ -18,10 +18,15 @@ namespace QuanLiTaiChinh
             System.Diagnostics.Debug.WriteLine(fundingNameInput.Text);
             System.Diagnostics.Debug.WriteLine(fundSpentInput.Text);
 
-
+            noticeLabel.Text = "Đã thêm thành công!!";
         }
 
         private void dateSelector_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
         {
 
         }
