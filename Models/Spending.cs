80370f186168b2a3ea9fe8c3d3c38b90e2c4ca23
Yukari <34794115+sorae42@users.noreply.github.com>
2023-05-30 23:08:48 +0700

    Create Spending.cs

---
+++ Models/Spending.cs
@@ -0,0 +1,23 @@
+﻿using QuanLiTaiChinh.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiTaiChinh.Models
+{
+    public class Spending 
+    {
+        public static int add(string name, int amount, string date, int profileID)
+        {
+            return (int)DataProvider.ExecuteNonQuery("INSERT INTO Spendings (spendName, spendDate, spendAmount, spendUser) VALUES ( @name , @date , @amount , @id )", name, date, amount, profileID);
+        }
+
+        public static DataTable? getAll(int profileID, string date)
+        {
+            return DataProvider.ExecuteReader("SELECT * FROM Spendings WHERE spendUser = @id AND spendDate = @date", profileID, date);
+        }
+    }
+}
